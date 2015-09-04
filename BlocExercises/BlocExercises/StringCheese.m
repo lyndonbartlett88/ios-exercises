71bc9181a0400dd7c2319276f6679fb6b49cdef7
Lyndon <lyndon@ksbartlett.com>
2015-09-04 12:01:24 -0500

    Completed Assignment 9
    Questions on numberOfCheesesStringWithCheeseCount method.

--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -12,13 +12,17 @@
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
+    return [NSString stringWithFormat:@"My favorite cheese is %@.", cheeseName];
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
+    NSString *returnString = cheeseName;
     if ([[cheeseName lowercaseString] hasSuffix:@" cheese"]) {
+        NSRange cheeseRange = [cheeseName rangeOfString:@" cheese" options:NSCaseInsensitiveSearch | NSBackwardsSearch];
+        returnString = [cheeseName stringByReplacingCharactersInRange:cheeseRange withString:@""];
         /* WORK HERE, ASSUMING `cheeseName` ENDS WITH " cheese" */
     } else {
+        NSLog(@"There is no cheese word.");
         /* WORK HERE, ASSUMING `cheeseName` DOES NOT END WITH " cheese" */
     }
 
@@ -26,21 +30,25 @@
      (You will learn more about if/else statements in a later checkpoint.)
      */
 
-    return nil;
+    return returnString;
 }
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
-    if (cheeseCount == 1) {
-        /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
-    } else {
-        /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
+   if (cheeseCount == 1) {
+//        NSLog(@"%d cheese", cheeseCount);
+//        /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
     }
-    
-    /*
-     (You will learn more about if/else statements in a later checkpoint.)
-     */
-    
-    return nil;
+//       else {
+//        NSLog(@"%d cheeses", cheeseCount);
+//        /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
+//    }
+//    /*
+//     (You will learn more about if/else statements in a later checkpoint.)
+//     */
+//    return nil;
+    NSString *cheeseString = (cheeseCount == 1 ? @"cheese" : @"cheeses");
+    return [NSString stringWithFormat:@"%d %@", cheeseCount, cheeseString];
+    // it's wanting to force cheesecount into an unsigned long, and change the %d to %lu. This whole method doesn't make sense to me. Also why is cheeseCount an NSUInteger? Where are they declaring cheeseCount, how does it know that it's 1?
 }
 
 @end
